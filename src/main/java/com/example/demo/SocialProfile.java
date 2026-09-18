966b6ae44136051172f34290d0abce2c4bd85d54
Ferdous-M <mahzabinferdous000@gmail.com>
2026-09-18 15:50:49 +0600

    adding models

---
+++ src/main/java/com/example/demo/SocialProfile.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SocialProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
